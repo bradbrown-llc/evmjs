a5e5c2e61b59a2fbaba71762cb73725ba29c9bb6
Brad Brown <bradbrown@bradbrown.llc>
2023-11-18 19:47:56 -0500

    export ESH

--- lib/ESH.ts
+++ lib/ESH.ts
@@ -1,7 +1,7 @@
 import { isNode, isChain, isChains, isChainsets } from './chain.ts'
 import Hermes from './Hermes.ts'
 
-class ESH implements EJRA {
+export default class ESH implements EJRA {
 
     rpc?: string
     node?: Node
@@ -23,15 +23,18 @@ class ESH implements EJRA {
     }
     
     get clientVersion() {
-        const rpc = this.rpc
-            ?? this.node?.rpc
-            ?? this.chain?.rpc[0]
-            ?? this.chains?.[0].rpc?.[0]
-            ?? this.chainsets?.[0]?.[0].rpc?.[0]
-        if (!(typeof rpc == 'string')) throw 'invalid rpc'
-        const method = 'web3_clientVersion'
-        
-        return Hermes.get(rpc, { method })
+        return (async () => {
+            const rpc = this.rpc
+                ?? this.node?.rpc
+                ?? this.chain?.rpc[0]
+                ?? this.chains?.[0].rpc?.[0]
+                ?? this.chainsets?.[0]?.[0].rpc?.[0]
+            if (!(typeof rpc == 'string')) throw 'invalid rpc'
+            const method = 'web3_clientVersion'
+            const response = await Hermes.get(rpc, { method })
+            if (!(typeof response == 'string')) throw 'invalid repsonse'
+            return response
+        })()
     }
     
 }
