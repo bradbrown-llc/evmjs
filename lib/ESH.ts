97d4d143e6e4ef58eed7e057513d69c6b09cb443
Brad Brown <bradbrown@bradbrown.llc>
2023-11-20 02:35:58 -0500

    clientVersion streaming multinode request with logging

--- lib/ESH.ts
+++ lib/ESH.ts
@@ -1,7 +1,7 @@
 import { isNode, isChain, isChains, isChainsets } from './chain.ts'
-import Hermes from './Hermes.ts'
+import Hermes from './Hermes.ts' 
 
-export default class ESH implements EJRA {
+export default class ESH implements Pick<EJRA, 'clientVersion'> {
 
     rpc?: string
     node?: Node
@@ -22,19 +22,52 @@ export default class ESH implements EJRA {
         else throw 'arg not Node|Chain|Chains|Chainsets'
     }
     
-    get clientVersion() {
-        return (async () => {
-            const rpc = this.rpc
-                ?? this.node?.rpc
-                ?? this.chain?.rpc[0]
-                ?? this.chains?.[0].rpc?.[0]
-                ?? this.chainsets?.[0]?.[0].rpc?.[0]
-            if (!(typeof rpc == 'string')) throw 'invalid rpc'
-            const method = 'web3_clientVersion'
-            const response = await Hermes.get(rpc, { method })
-            if (!(typeof response == 'string')) throw 'invalid repsonse'
-            return response
-        })()
+    #call({ method, params, dry }: { method:string, params?:unknown[], dry?: boolean }) {
+        const rpc = this.rpc
+            ?? this.node?.rpc
+            ?? this.chain?.rpc
+            ?? this.chains?.[0].rpc?.[0]
+            ?? this.chainsets?.[0]?.[0].rpc?.[0]
+        if (rpc === undefined) throw 'undefined rpc'
+        return Hermes.call(rpc, { method, params, dry })
     }
+    
+    async clientVersion(dry?:boolean) {
+        console.log('ESH: acquiring AIQ from Hermes')
+        const response = this.#call({ method: 'web3_clientVersion', dry })
+        function isRecord(x: unknown): x is Record<string,string> { x; return !!dry }
+        if (isRecord(response)) return response as Record<string,string>
+        const responses:Record<string,string|Error> = {}
+        console.log('ESH: pulling objects from AIQ')
+        if (!response[Symbol.asyncIterator]) throw '???'
+        for await (const { url, res, status: _ } of response) {
+            console.log('ESH: pulled value from queue', { url, res })
+            if (res instanceof Error) responses[url] = res
+            else if (typeof res == 'string') responses[url] = res
+            else responses[url] = new Error(`invalid type: ${JSON.stringify(res)} not string`)
+        }
+        console.log('ESH: returning response record')
+        return responses
+    }
+    
+    // async blockNumber(): Promise<bigint>
+    // async blockNumber({ dry }:{ dry:true }): Promise<string>
+    // async blockNumber({ dry }:{ dry?:boolean }={}) {
+    //     const response = await this.#call({ method: 'eth_blockNumber', dry }).catch(e => e instanceof Error ? e : new Error(e))
+    //     if (response instanceof Error) throw response
+    //     if (dry) return response as string
+    //     if (typeof response != 'string') throw new Error(`invalid response: ${JSON.stringify(response)}`)
+    //     return BigInt(response)
+    // }
+    
+    // async getLogs(filter:Filter):Promise<Log[]>
+    // async getLogs(filter:Filter, { dry }:{ dry:true }):Promise<string>
+    // async getLogs(filter:Filter, { dry }:{ dry?:boolean }={}) {
+    //     const response = await this.#call({ method: 'eth_getLogs', params: [filter], dry }).catch(e => e instanceof Error ? e : new Error(e))
+    //     if (dry) return response as string
+    //     if (response instanceof Error) throw response
+    //     if (!(response instanceof Array)) throw new Error(`invalid response: ${JSON.stringify(response)}`)
+    //     return response
+    // }
     
 }
