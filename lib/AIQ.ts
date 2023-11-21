74f7da1bd5f22b72c10f2df5a0ce76b2c8fd6a6a
Brad Brown <bradbrown@bradbrown.llc>
2023-11-21 01:14:18 -0500

    remove logging from AIQ

--- lib/AIQ.ts
+++ lib/AIQ.ts
@@ -6,24 +6,18 @@ export default class AIQ<T> implements AsyncIterator<T>{
     terminator: symbol
 
     constructor() {
-        console.log('AIQ: constructing')
         this.#resetPromise()
         this.queue = []
         this.terminator = Symbol()
     }
     
     async next():Promise<IteratorResult<T>> {
-        console.log('AIQ: next called, checking queue')
         if (!this.queue.length) { await this.promise; this.#resetPromise() }
-        console.log('AIQ: items in queue or push called')
         const value = this.queue.shift() as T|symbol
-        console.log('AIQ: value shifted from queue')
         if (typeof value == 'symbol') {
-            console.log('AIQ: terminator found')
             if (value === this.terminator) return { done: true, value: null }
             else throw 'invalid terminator symbol'
         }
-        console.log('AIQ: returning value')
         return { value }
     }
     
