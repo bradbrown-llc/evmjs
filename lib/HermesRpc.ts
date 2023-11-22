b87716c09c83d1e16123c41deee3b77c5ce220b2
Brad Brown <bradbrown@bradbrown.llc>
2023-11-22 01:30:00 -0500

    Hermes specifically for RPC, not too interesting (until we get to HermesChain)

--- lib/HermesRpc.ts
+++ lib/HermesRpc.ts
@@ -1,15 +1,9 @@
-import rethrow from './rethrow.ts'
-import { getInit, getBody } from './Hermes.ts'
-import { isEjraRes } from './EJRA.ts'
+import JsonRpc from './JsonRpc.ts'
 
 export default class Hermes {
 
-    static async call(url:string, opts:HermesOpts) {
-        if (opts.dry === true) return getBody(opts)
-        const repsonse = await fetch(url, getInit(opts)).catch(rethrow)
-        const json = await repsonse.json().catch(rethrow)
-        if (!isEjraRes(json)) throw new Error('response shape not EjraRes')
-        return json
+    static request(url:string, req:JsonRpcReq|JsonRpcReqs) {
+        return JsonRpc.request(url, req)
     }
 
 }
