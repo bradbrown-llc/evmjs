c1f12e2ba301f3bf7544c8eb9006b7c9c8fd5e1e
Brad Brown <bradbrown@bradbrown.llc>
2023-12-01 01:51:41 -0500

    limit option for AIQ, if many pushing and none know when to term

--- lib/AIQ.ts
+++ lib/AIQ.ts
@@ -10,18 +10,22 @@ export default class AIQ<T> implements AsyncIterator<T>{
     terminator: symbol
     #promise!: Promise<unknown>
     #queue: Array<T|symbol>
-
-    constructor() {
+    limit?:number
+    
+    constructor(o?:{ limit?:number }) {
         this.#resetPromise()
         this.#queue = []
+        if (o?.limit) this.limit = o.limit
         this.terminator = Symbol()
     }
     
     async next():Promise<IteratorResult<T>> {
         if (!this.#queue.length) { await this.#promise; this.#resetPromise() }
         const value = this.#queue.shift() as T|symbol
-        if (typeof value == 'symbol') return { done: true, value: null }
-        else return { value }
+        if (value === this.terminator) return { done: true, value: null }
+        if (this.limit === 0) { return { done: true, value: null } }
+        if (this.limit) { this.limit--; if (!this.limit) this.push(this.terminator) }
+        return { value } as { value:T }
     }
     
     [Symbol.asyncIterator]() { return this }
