22537c7840cd3e5dc2a1b361b4c24fe92028d727
Brad Brown <bradbrown@bradbrown.llc>
2023-12-01 01:53:32 -0500

    add, use, export isObject guard

--- lib/guards.ts
+++ lib/guards.ts
@@ -3,12 +3,12 @@ function isBigIntable(x:unknown):x is BigIntable {
     if (typeof x == 'string' || typeof x == 'number') try {
         return BigInt(x) !== undefined } catch (_) { return false } else return false }
 function isArray(x:unknown): x is Array<unknown> { return x instanceof Array }
-// function isObject(x:unknown): x is object { return typeof x == 'object' && x !== null }
+function isObject(x:unknown): x is object { return typeof x == 'object' && x !== null }
 function isJsonRpcRes(x:unknown): x is JsonRpcRes {
-    return typeof x == 'object' && x !== null
+    return isObject(x)
         && 'jsonrpc' in x && x.jsonrpc == '2.0'
         && ('result' in x || 'error' in x)
         && 'id' in x && typeof x.id == 'number'
 }
 
-export { isString, isBigIntable, isArray/*, isObject*/, isJsonRpcRes }
+export { isString, isBigIntable, isArray, isObject, isJsonRpcRes }
