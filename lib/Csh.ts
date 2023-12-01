6b60646341062f4a4952077344d4b69e3a9ada2a
Brad Brown <bradbrown@bradbrown.llc>
2023-12-01 01:52:46 -0500

    nsh methods reflect on csh, 3 increasingly interesting csh methods/getters

--- lib/Csh.ts
+++ lib/Csh.ts
@@ -1,17 +1,43 @@
 import Nsh from './Nsh.ts'
+import AIQ from './AIQ.ts'
+import AbortController2 from './AbortController2.ts'
 
+interface Csh extends Nsh { nshs:Nsh[] }
 class Csh {
-    nshs:Array<Nsh>
-    constructor(chain:{ rpc:Array<string> }) {
+    nshs:Nsh[]
+    constructor(chain:{ rpc:string[] }) {
+        if (!chain.rpc.length) throw 'no rpcs in chain'
         this.nshs = chain.rpc.map(url => new Nsh(url))
+        const { constructor:_, ...descriptors } = Object.getOwnPropertyDescriptors(Nsh.prototype)
+        // we want every method of Nsh to be available on Csh (besides constructor)
+        // where using the method uses it on every Nsh of Csh.nshs
+        for (const [key, desc] of Object.entries(descriptors) as [keyof Nsh,PropertyDescriptor][]) {
+            const fn = (...p:unknown[]) => { for (const nsh of this.nshs) (nsh[key] as (...p:unknown[]) => unknown)(...p); return this }
+            desc.value = fn
+            Object.defineProperty(this, key, desc)
+        }
     }
-    send() {
-        for (const nsh of this.nshs) nsh.send()
-        return this
+    // get [q]ueue of [l]atest network request's ixs
+    get ql() {
+        const aiq = new AIQ<Ix[]>({ limit: this.nshs.length })
+        for (const nsh of this.nshs) {
+            const ixs:Ix[] = []
+            for (const ix of nsh.ixs)
+                if (ix.req.id < (ixs.at(-1)?.req?.id ?? Infinity))
+                    ixs.push(ix)
+                else break
+            nsh.fetches[0].then(() => aiq.push(ixs))
+        }
+        return aiq
     }
-    clientVersion() { for (const nsh of this.nshs) nsh.clientVersion(); return this }
-    blockNumber() { for (const nsh of this.nshs) nsh.blockNumber(); return this }
-    getLogs(filter:Filter) { for (const nsh of this.nshs) nsh.getLogs(filter); return this }
+    // send and return [ [q]ueue of [l]atest network request's ixs, abort (to abort the send) ]
+    sendql(o?:{ timeout:number }) {
+        const controller = new AbortController2(o)
+        const signal = controller.signal2
+        this.send({ signal })
+        return [this.ql, () => controller.abort()] as const
+    }
+    sendfold(fn:(aiq:AIQ<Ix[]>, abort:() => void) => unknown, o?:{ timeout:number }) { return fn(...this.sendql(o)) }
 }
 
 export default Csh
