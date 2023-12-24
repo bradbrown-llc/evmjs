0e7707451325408c2347de65539ba79b73c3dd0a
Brad Brown <bradbrown@bradbrown.llc>
2023-12-24 12:17:52 -0500

    ejra zod schema complex barrel

---
+++ ejra/lib/z/cplx/index.ts
@@ -0,0 +1,4 @@
+export { default as TxR} from './TxR.ts'
+export { default as BlockResponse } from './BlockResponse.ts'
+export { default as LogR } from './LogR.ts'
+export { default as RcptR } from './RcptR.ts'
